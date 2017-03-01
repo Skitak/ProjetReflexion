7984589010f9f9fae55640237ed4a003cae5a0cb
Bastien BOUQUIN <bouquin@iut.univ-paris5.fr>
2017-03-01 12:05:08 +0100

    Client done ? Serveur slightly started

--- src/client/App.java
+++ src/client/App.java
@@ -1,5 +1,34 @@
 package client;
 
-public class App {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.Scanner;
 
+import serveur.Serveur;
+
+public class App {
+	public static void main(String[] args) {
+		Socket s = null;		
+		try {
+			s = new Socket(Serveur.IP_ADDR, Serveur.PORT_CONNEXION);
+			new Reader(s).start();
+			PrintWriter out = new PrintWriter (s.getOutputStream ( ), true);
+			@SuppressWarnings("resource")
+			Scanner clavier = new Scanner(System.in);
+			
+			String line;
+			while(true) {
+				line = clavier.nextLine();
+				out.println(line);
+			}
+		}
+		catch (IOException e) {
+			
+		}
+		try {s.close(); } 
+		catch (IOException e2) { }		
+	}
 }
--- src/serveur/Serveur.java
+++ src/serveur/Serveur.java
@@ -1,10 +1,20 @@
 package serveur;
 
+import java.util.TreeMap;
+
+import utilisateurs.Amateur;
+
 public class Serveur {
-
+	
+	public static final int PORT_CONNEXION = 2700;
+	public static final String IP_ADDR = "localhost";
+	private static TreeMap<String, Amateur> users;
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		users = new TreeMap<>();
+	}
+	
+	public static void addUser(Amateur user){
 	}
 
 }
